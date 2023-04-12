09a222f59ff86aee0aed995fc7b33e24968e4563
MikeTheMerchant <mikethemerchant@gmail.com>
2023-04-12 19:30:01 +0000

    Update App.js to mark

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>this is mike</h2>
+      <h2>this is Mark</h2>
     </div>
   );
 }
